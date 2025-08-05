799f997ca851fa32d81fd38ff2dbadb176197912
Rujinopy <ruth3442@gmail.com>
2025-08-05 22:32:57 +0700

    refactor: adjust animation translate values for fadeInUp and fadeInDown effects

--- tailwind.config.js
+++ tailwind.config.js
@@ -135,11 +135,11 @@ module.exports = {
           "100%": { opacity: 1 },
         },
         fadeInUp: {
-          "0%": { opacity: 0, transform: "translateY(2rem)" },
+          "0%": { opacity: 0, transform: "translateY(0.5rem)" },
           "100%": { opacity: 1, transform: "translateY(0)" },
         },
         fadeInDown: {
-          "0%": { opacity: 0, transform: "translateY(-2rem)" },
+          "0%": { opacity: 0, transform: "translateY(-1rem)" },
           "100%": { opacity: 1, transform: "translateY(0)" },
         },
         fadeInRight: {
