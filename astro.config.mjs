bfc9b504475c8c4dc72300c6dc0cbf83e2fd4c43
Rujinopy <ruth3442@gmail.com>
2025-08-28 22:50:42 +0700

    feat: add Decap CMS integration with initial configuration
    - Updated package.json to include decap-cms-app dependency, astrojs/vercel.
    - Created admin configuration file for Decap CMS with blog collection setup.
    - Added admin HTML page to serve as the content manager interface.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,9 @@ import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
-// import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,7 +20,7 @@ export default defineConfig({
   prefetch: {
     prefetchAll: true
   },
-  // adapter: cloudflare(),
+  adapter: vercel(),
   integrations: [react(), sitemap(), tailwind({
     config: {
       applyBaseStyles: false
